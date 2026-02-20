290d4612dac683ad2287f5fb674071265b9689e8
Vaibhav <vaibhavranjan420@gmail.com>
2026-02-20 18:47:21 +0530

    backend setup done

---
+++ server/src/routes/incidentRoutes.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  createIncident,
+  getAllIncidents,
+  getIncidentById,
+} from "../controllers/incidentController";
+
+const router = express.Router();
+
+router.post("/create", createIncident);
+
+router.get("/all", getAllIncidents);
+
+router.get("/:id", getIncidentById);
+
+export default router;
